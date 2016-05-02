f4c43eb5a56e054aceff6af70d6cd323187a571b
John <mountmckiki@gmail.com>
2016-05-02 11:09:46 -0400

    Continued learning in Angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 (function(){
   var app = angular.module('store', []);
 
-  app.controller('StoreCtrl', function(){
+  // This handles the items in the store
+  app.controller('StoreController', function(){
     this.products = gems;
   });
 
+  // This is the array of information from the items in the store
   var gems = [
     {
       name: 'Dodecahedron',
@@ -20,6 +22,18 @@
           full: 'dodecahedron-02-full.jpg',
           thumb: 'dodecahedron-02-thumb.jpg'
         }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product sucks",
+          author: "tim@hater.com"
+        }
       ]
     },
     {
@@ -30,15 +44,29 @@
     }
   ];
 
-  app.controller('PanelCtrl', function(){
+  app.controller('PanelController', function(){
+    // This sets the default description tab to the first tab on page load
     this.tab = 1;
 
+    // This sets the tab to the current tab selected and adds styling to display that
     this.selectTab = function(setTab) {
       this.tab = setTab;
     };
 
     this.isSelected = function(checkTab) {
       return this.tab === checkTab;
+    };
+  });
+
+  app.controller('ReviewController', function(){
+    // This initailizes a new review object
+    this.review = {};
+
+    this.addReview = function(product){
+      // This submits the review form to the array
+      product.reviews.push(this.review);
+      // This clears the form after it has been submitted
+      this.review = {};
     };
   });
 }) ();
